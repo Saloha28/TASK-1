8205da5449d6afd444fc1037dc1d0cafe5cbde48
saloha <132863399+Saloha28@users.noreply.github.com>
2024-08-28 16:34:28 +0300

    Add files via upload

---
+++ Task.js
@@ -0,0 +1,39 @@
+// let x = 10; 
+
+// function ZSC (){
+//     let x = 20 ; 
+//     console.log(x)
+// }
+// ZSC () // output => 20 local Scope 
+
+ /////////////////////////////////////
+
+
+// let y = 10; 
+
+// function ZSC (){
+//     console.log(y)
+// }
+// ZSC() // 10 Global Scope
+
+// /////////////////
+
+// var z = 17; 
+
+// function ZSC (){
+//     var z = 50 ;
+//     console.log(z)
+// }
+// ZSC() // 50 Local Group
+
+/////////////////////
+
+// let a = 15; 
+
+// function ZSC (){
+    
+//     console.log(a)
+// }
+// ZSC() // 15 Global Group
+
+
